563a07251894c7c54c722b0a41885cca69efb8f6
= <=>
2013-12-02 00:47:44 +0900

    スクレイピング対象のURL追加

--- db/migrate/20131130043025_create_contents.rb
+++ db/migrate/20131130043025_create_contents.rb
@@ -1,8 +1,8 @@
 class CreateContents < ActiveRecord::Migration
   def change
     create_table :contents do |t|
-      t.string :image_url
       t.string :url
+      t.text :image_url
       t.text :title
 
       t.timestamps
--- lib/tasks/insert_contents_task.rb
+++ lib/tasks/insert_contents_task.rb
@@ -10,6 +10,17 @@ require "nokogiri"
 class Tasks::InsertContentsTask
   # To change this template use File | Settings | File Templates.
   def self.execute_mery
+    #TODO:主要サイトの洗い出しとそれに合わせての検索結果をどうするかを決める。
+    #TODO:テーブルにどこまでデータを保持するかを検討する必要がある。。
+
+
+    #ヘアーアレンジなどの可愛め系(mery)
+    #url = ['http://mery.jp/hairstyle','http://mery.jp/love','http://mery.jp/gourmet']
+    #時短系(nanapi,peachy)
+    #url = ['http://nanapi.jp/search/q:%E6%99%82%E7%9F%AD/rank:1/theme_id:626','http://news.livedoor.com/search/article/?ie=euc-jp&word=%BB%FE%C3%BB%A1%A1%C0%E1%CC%F3']
+    #可愛い系(style 4 designe)
+    #url = ['http://design.style4.info/?s=%E3%82%AB%E3%83%AF%E3%82%A4%E3%82%A4&submit=Search','http://design.style4.info/?s=%E7%B5%B6%E6%99%AF&submit=Search']
+
     url = 'http://mery.jp/hairstyle'
 
     charset = nil
