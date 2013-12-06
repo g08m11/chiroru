1ef492bee5e801f0a72b7c77bf78a5c85796a472
Gushiken Masaru <gushiken@5smile.com>
2013-12-06 19:11:20 +0900

    API取得

--- lib/tasks/insert_contents_task.rb
+++ lib/tasks/insert_contents_task.rb
@@ -1,6 +1,10 @@
+require 'active_support'
 require "open-uri"
 require "rubygems"
 require "nokogiri"
+require 'nkf'
+require 'pp'
+
 
 
 #検索結果ではURL単一しか取れないから
@@ -86,6 +90,28 @@ class Tasks::InsertContentsTask
       end
 
     end
+
+  end
+
+  def self.execute_api
+    #TODO:主要サイトの洗い出しとそれに合わせての検索結果をどうするかを決める。
+    #TODO:テーブルにどこまでデータを保持するかを検討する必要がある。。
+    #TODO:ある程度のサイトは一つのモジュールで対応可能なためパラメータや条件で取得を変えるようにしたい。
+
+
+    #ヘアーアレンジなどの可愛め系(mery)
+    #url = ['http://mery.jp/hairstyle','http://mery.jp/love','http://mery.jp/gourmet']
+    #時短系(nanapi,peachy)
+    #nanapiは画像が汚いので取り扱わない
+    #url = ['http://nanapi.jp/search/q:%E6%99%82%E7%9F%AD/rank:1/theme_id:613','http://news.livedoor.com/search/article/?ie=euc-jp&word=%BB%FE%C3%BB%A1%A1%C0%E1%CC%F3']
+    #可愛い系(style 4 designe)
+    #url = ['http://design.style4.info/?s=%E3%82%AB%E3%83%AF%E3%82%A4%E3%82%A4&submit=Search','http://design.style4.info/?s=%E7%B5%B6%E6%99%AF&submit=Search']
+
+    #url = 'http://nanapi.jp/search/q:%E6%99%82%E7%9F%AD/rank:1/theme_id:613'
+    #url = 'http://webservice.recruit.co.jp/beauty/salon/v1?order=3&name=%E3%82%B5%E3%83%AD%E3%83%B3&key=d80cc5011c92e61d'
+
+    h = Hash.from_xml(open('http://webservice.recruit.co.jp/beauty/salon/v1?order=3&name=%E3%82%B5%E3%83%AD%E3%83%B3&key=d80cc5011c92e61d').read)
+    pp h['results']['salon'].first['id']
 
   end
 end
