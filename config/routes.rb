1217194be8d1dd609b939ccae735a10fa1a2f929
Gushiken Masaru <gushiken@5smile.com>
2013-12-07 12:45:56 +0900

    roots修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Chiroru::Application.routes.draw do
   resources :contents
 
+  root :to => 'contents#index'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
